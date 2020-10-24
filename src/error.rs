b41f03dbcf8a95e28fb6a08f9cbeddd615427d7d
Maxim Zhiburt <zhiburt@gmail.com>
2020-10-24 23:22:11 +0300

    Init

---
+++ src/error.rs
@@ -0,0 +1,17 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PostalError {
+    #[error("data store disconnected")]
+    Network(#[from] reqwest::Error),
+    #[error("data store disconnected")]
+    UrlIssue(#[from] url::ParseError),
+    #[error("send error({code:?}): {message:?}")]
+    Error { code: String, message: String },
+    #[error("internal error on postal side")]
+    InternalServerError,
+    #[error("postal server unavailable")]
+    ServiceUnavailableError,
+    #[error("Request should likely be sent to an another URL")]
+    ExpectedAlternativeUrl,
+}
